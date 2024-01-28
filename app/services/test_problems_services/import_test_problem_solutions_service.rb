1f6aafa97a2b3f2047055936f1ee7ed24a534bb6
Umar Alkafaween <omar.ka923@gmail.com>
2024-01-28 13:59:10 +0300

    Update score column name

--- app/services/test_problems_services/import_test_problem_solutions_service.rb
+++ app/services/test_problems_services/import_test_problem_solutions_service.rb
@@ -21,7 +21,7 @@ module TestProblemsServices
           try: row['try'], code: row['answer'], submission_time: Time.at(row['time'].to_i)
         )
 
-        solution_test_suite_grade = solution.solution_test_suite_grades.build(test_suite_id: @test_suite.id, grade: row['rawfracion'].to_f)
+        solution_test_suite_grade = solution.solution_test_suite_grades.build(test_suite_id: @test_suite.id, grade: row['rawfraction'].to_f)
 
         unless solution.save
           errors.add(:base, "Could not add solution: (#{solution.student_unique_reference}, #{solution.try}) from file #{@problem_file}")
